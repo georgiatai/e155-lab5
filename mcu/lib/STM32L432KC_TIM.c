66ee0679d3c56a05397537d8603638951a35b132
georgiatai <ytai@g.hmc.edu>
2025-10-08 03:03:09 -0700

    lab5 working not polling

--- mcu/lib/STM32L432KC_TIM.c
+++ mcu/lib/STM32L432KC_TIM.c
@@ -4,23 +4,35 @@
 #include "STM32L432KC_TIM.h"
 #include "STM32L432KC_RCC.h"
 
-void initTIM(TIM_TypeDef * TIMx){
-  // Set prescaler to give 1 ms time base
-  uint32_t psc_div = (uint32_t) ((SystemCoreClock/1e3));
+void initTIM(TIM_TypeDef *TIMx) {
+    // Set prescaler value
+    TIMx->PSC = 4999;
 
-  // Set prescaler division factor
-  TIMx->PSC = (psc_div - 1);
-  // Generate an update event to update prescaler value
-  TIMx->EGR |= 1;
-  // Enable counter
-  TIMx->CR1 |= 1; // Set CEN = 1
+    // Clear UG bit (TIMx_EGR[0]) and setting it to generate an update event
+    TIMx->EGR  &= ~(1 << 0);
+    TIMx->EGR |= (1 << 0); 
+
+    // Counter enable (CEN, TIMx_CR1[0])
+    TIMx->CR1 |= (1 << 0);
 }
 
-void delay_millis(TIM_TypeDef * TIMx, uint32_t ms){
-  TIMx->ARR = ms;// Set timer max count
-  TIMx->EGR |= 1;     // Force update
-  TIMx->SR &= ~(0x1); // Clear UIF
-  TIMx->CNT = 0;      // Reset count
+// Delay in milliseconds using TIMx
+void delay_millis(TIM_TypeDef * TIMx, uint32_t ms) {
+    // Set auto-reload register (TIMx_ARR[15:0])
+    // arr_val = wait_time / 1000 * freq_TIM
+    int arr_val = ms * (80000 / (4999+1));
+    TIMx->ARR = arr_val;
 
-  while(!(TIMx->SR & 1)); // Wait for UIF to go high
+    // Clear UG bit (TIMx_EGR[0]) and setting it to generate an update event
+    TIMx->EGR  &= ~(1 << 0);
+    TIMx->EGR |=  (1 << 0);
+
+    // Clear UIF (TIMx_SR[0])
+    TIMx->SR  &= ~(1 << 0);
+
+    // Reset count (TIMx_CNT[15:0])
+    TIMx->CNT = 0;
+  
+    // Wait for update event with UIF (TIMx_SR[0]) set
+    while(((TIMx->SR) & 1U) != 1);
 }
--- mcu/src/main.c
+++ mcu/src/main.c
@@ -1,11 +1,23 @@
+/*********************************************************************
+*                    SEGGER Microcontroller GmbH                     *
+*                        The Embedded Experts                        *
+**********************************************************************
 
-#include <stm32l432xx.h>
-#include <stdio.h>
+-------------------------- END-OF-HEADER -----------------------------
 
-#define A_IN (GPIOA->IDR & (1 << 6)) // PA6
-#define B_IN (GPIOA->IDR & (1 << 7)) // PA7
+File    : main.c
+Purpose : Generic application start
 
-uint32_t quad_count = 0;
+*/
+
+#include "STM32L432KC.h"
+
+#define A_IN digitalRead(PA9)  // PA9
+#define B_IN digitalRead(PA10) // PA10
+
+#define _VAL2FLD(field, value) (((uint32_t)(value) << field ## _Pos) & field ## _Msk)
+
+signed int quad_count = 0;
 uint32_t direction  = 0;   // 0 is clockwise, 1 is counter-clockwise
 float    ang_vel    = 0.0; // Angular velocity in rev/s
 
@@ -13,79 +25,75 @@ float    ang_vel    = 0.0; // Angular velocity in rev/s
 int main(void) {
     configureFlash();
     configureClock();
-	
+
+    // 1. Enable SYSCFG clock domain in RCC
+    RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN;	
     // Setting up clocks
     RCC->AHB2ENR |= (1 << 0);  // GPIOA
     RCC->APB2ENR |= (1 << 16); // TIM15
 
     // Initialize timer
     initTIM(TIM15);
-    TIM15->DIER |= _VAL2FLD(TIM15_DIER_UIE, 0b1);
+    TIM15->DIER |= (1 << 0);
 
     // Setting up GPIOA
     gpioEnable(GPIO_PORT_A);
-    pinMode(PA6, GPIO_INPUT); // PA6 as input
-    pinMode(PA7, GPIO_INPUT); // PA7 as input
+    pinMode(PA9, GPIO_INPUT); // PA9 as input
+    pinMode(PA10, GPIO_INPUT); // PA10 as input
+    
+    GPIOA->PUPDR |= _VAL2FLD(GPIO_PUPDR_PUPD9, 0b00); // Set PA9 as pull-up
+    GPIOA->PUPDR |= _VAL2FLD(GPIO_PUPDR_PUPD9, 0b01); // Set PA9 as pull-up
+    GPIOA->PUPDR |= _VAL2FLD(GPIO_PUPDR_PUPD10, 0b00); // Set PA9 as pull-up
+    GPIOA->PUPDR |= _VAL2FLD(GPIO_PUPDR_PUPD10, 0b01); // Set PA10 as pull-up
 
-    GPIOA->PUPDR |= _VAL2FLD(GPIO_PUPDR_PUPD6, 0b01); // Set PA6 as pull-up
-    GPIOA->PUPDR |= _VAL2FLD(GPIO_PUPDR_PUPD7, 0b01); // Set PA7 as pull-up
 
-    // 1. Enable SYSCFG clock domain in RCC
-    RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN;
     // 2. Configure EXTICR for the input button interrupt
-    SYSCFG->EXTICR[1] |= _VAL2FLD(SYSCFG_EXTICR1_EXTI2, 0b000); // Select PA2
+    SYSCFG->EXTICR[2] |= _VAL2FLD(SYSCFG_EXTICR3_EXTI9, 0b000); // Select PA2
+    SYSCFG->EXTICR[2] |= _VAL2FLD(SYSCFG_EXTICR3_EXTI10, 0b000); // Select PA2
 
     // Enable interrupts globally
     __enable_irq();
 
     // TODO: Configure interrupt for falling edge of GPIO pin for button
     // 1. Configure mask bit
-    EXTI->IMR1 |= (1 << gpioPinOffset(BUTTON_PIN)); // Configure the mask bit
+    EXTI->IMR1 |= (1 << gpioPinOffset(PA9)); // Configure the mask bit
+    EXTI->IMR1 |= (1 << gpioPinOffset(PA10)); // Configure the mask bit
     // 2. Disable rising edge trigger
-    EXTI->RTSR1 &= ~(1 << gpioPinOffset(BUTTON_PIN));// Disable rising edge trigger
+    EXTI->RTSR1 |= (1 << gpioPinOffset(PA9)); // Disable rising edge trigger
+    EXTI->RTSR1 |= (1 << gpioPinOffset(PA10));// Disable rising edge trigger
     // 3. Enable falling edge trigger
-    EXTI->FTSR1 |= (1 << gpioPinOffset(BUTTON_PIN));// Enable falling edge trigger
+    EXTI->FTSR1 |= (1 << gpioPinOffset(PA9));// Enable falling edge trigger
+    EXTI->FTSR1 |= (1 << gpioPinOffset(PA10));// Enable falling edge trigger
     // 4. Turn on EXTI interrupt in NVIC_ISER
-    NVIC->ISER[0] |= (1 << EXTI6_IRQn);
-    NVIC->ISER[0] |= (1 << EXTI7_IRQn);
+    __NVIC_EnableIRQ(EXTI9_5_IRQn);
+    __NVIC_EnableIRQ(EXTI15_10_IRQn);
+
 
 
     while(1){
-        delay_millis(TIM15, 1000); // Delay 1 second
-        ang_vel = (float) quad_count / 400.0; // Calculate angular velocity in rev/s
-        quad_count = 0; // Reset count
-    };
+      ang_vel = quad_count / (4.0*408.0); 
+      printf("Angular velocity: %.3f rev/s, Direction: %s\n", ang_vel, direction ? "CCW" : "CW");
+      quad_count = 0; // Reset count
+      delay_millis(TIM15, 1000);
+      
+      
+    }
 
 }
 
 void EXTI9_5_IRQHandler(void){
     // Check that the button was what triggered our interrupt
-    if (EXTI->PR1 & (1 << 6)){
+    if (EXTI->PR1 & (1 << 9)){
+        // printf("PA9, A_IN %d, B_IN %d \n", digitalRead(PA9), digitalRead(PA10));
         // If so, clear the interrupt (NB: Write 1 to reset.)
-        EXTI->PR1 |= (1 << 6);
+        EXTI->PR1 |= (1 << 9);
 
         // Then toggle the LED
-        if (A_IN && !B_IN) {
+        if (A_IN != B_IN) {
             // Clockwise
             quad_count++;
             direction = 0;
-        } else if (!A_IN && B_IN) {
-            // Counterclockwise
-            quad_count--;
-            direction = 1;
-        }
-    }
-
-    if (EXTI->PR1 & (1 << 7)){
-        // If so, clear the interrupt (NB: Write 1 to reset.)
-        EXTI->PR1 |= (1 << 7);
-
-        // Then toggle the LED
-        if (A_IN && !B_IN) {
-            // Clockwise
-            quad_count++;
-            direction = 0;
-        } else if (!A_IN && B_IN) {
+        } else {
             // Counterclockwise
             quad_count--;
             direction = 1;
@@ -94,14 +102,25 @@ void EXTI9_5_IRQHandler(void){
     }
 }
 
-void TIM1_UP_TIM15_IRQHandler(void){
+void EXTI15_10_IRQHandler(void){
+  
     // Check that the button was what triggered our interrupt
-    if (TIM15->SR & (1 << 0)){
+    if (EXTI->PR1 & (1 << 10)){
+        // printf("PA10, A_IN %d, B_IN %d \n", digitalRead(PA9), digitalRead(PA10));
         // If so, clear the interrupt (NB: Write 1 to reset.)
-        TIM15->SR |= (1 << 0);
+        EXTI->PR1 |= (1 << 10);
 
-        printf("Angular velocity: %.2f rev/s, Direction: %s\n", ang_vel, direction ? "CCW" : "CW");
-        quad_count = 0; // Reset count
+        // Then toggle the LED
+        if (A_IN == B_IN) {
+            // Clockwise
+            quad_count++;
+            direction = 0;
+        } else {
+            // Counterclockwise
+            quad_count--;
+            direction = 1;
+        }
+
     }
 }
 
