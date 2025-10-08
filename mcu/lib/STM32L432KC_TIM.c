c617c4764ad4315837bc527128920764224bd66b
georgiatai <161803507+georgiatai@users.noreply.github.com>
2025-10-07 19:03:26 -0700

    getting files from tutorial

--- mcu/lib/STM32L432KC_TIM.c
+++ mcu/lib/STM32L432KC_TIM.c
@@ -1,90 +1,26 @@
-/////////////////////////
-// STM32L432KC_TIM.c
-// Author: Georgia Tai, ytai@g.hmc.edu
-// Date: Sep 28, 2025
-// 
-// Source code for TIM functions, including initialization, delay, and PWM generation.
-/////////////////////////
+// STM32F401RE_TIM.c
+// TIM functions
 
 #include "STM32L432KC_TIM.h"
-#include <stdio.h>
+#include "STM32L432KC_RCC.h"
 
-// Initialize timer for basic counting (delay)
-void initTIM(TIMx_TypeDef *TIMx, uint32_t psc_val) {
-    // Set prescaler value
-    TIMx->PSC = psc_val;
+void initTIM(TIM_TypeDef * TIMx){
+  // Set prescaler to give 1 ms time base
+  uint32_t psc_div = (uint32_t) ((SystemCoreClock/1e3));
 
-    // Clear UG bit (TIMx_EGR[0]) and setting it to generate an update event
-    TIMx->EGR  &= ~(1 << 0);
-    TIMx->EGR |= (1 << 0); 
-
-    // Counter enable (CEN, TIMx_CR1[0])
-    TIMx->CR1 |= (1 << 0);
+  // Set prescaler division factor
+  TIMx->PSC = (psc_div - 1);
+  // Generate an update event to update prescaler value
+  TIMx->EGR |= 1;
+  // Enable counter
+  TIMx->CR1 |= 1; // Set CEN = 1
 }
 
-// Delay in milliseconds using TIMx
-void delay_millis(TIMx_TypeDef * TIMx, uint32_t ms, uint32_t psc_val) {
-    // Set auto-reload register (TIMx_ARR[15:0])
-    // arr_val = wait_time / 1000 * freq_TIM
-    int arr_val = ms * (80000 / (psc_val+1));
-    TIMx->ARR = arr_val;
+void delay_millis(TIM_TypeDef * TIMx, uint32_t ms){
+  TIMx->ARR = ms;// Set timer max count
+  TIMx->EGR |= 1;     // Force update
+  TIMx->SR &= ~(0x1); // Clear UIF
+  TIMx->CNT = 0;      // Reset count
 
-    // Clear UG bit (TIMx_EGR[0]) and setting it to generate an update event
-    TIMx->EGR  &= ~(1 << 0);
-    TIMx->EGR |=  (1 << 0);
-
-    // Clear UIF (TIMx_SR[0])
-    TIMx->SR  &= ~(1 << 0);
-
-    // Reset count (TIMx_CNT[15:0])
-    TIMx->CNT = 0;
-  
-    // Wait for update event with UIF (TIMx_SR[0]) set
-    while(((TIMx->SR) & 1U) != 1);
-}
-
-// Initialize timer for PWM generation
-void initPWM(TIMx_TypeDef *TIMx, uint32_t psc_val) {
-    // Set prescaler value
-    TIMx->PSC = psc_val;
-
-    // Set output 1 polarity to active high
-    TIMx->CCER |= (1 << 3);
-
-    // Enable output on channel 1
-    TIMx->CCER |= (1 << 0);
-
-    // Set mode to PWM mode 1 (110) on channel 1
-    TIMx->CCMR1 &= ~(0b111 << 4);
-    TIMx->CCMR1 |=  (0b110 << 4);
-
-    // Enable preload for CCR1 later for dutying cycle updates
-    TIMx->CCMR1 |= (1 << 3);
-
-    // Enable main output
-    TIMx->BDTR |= (1 << 15);
-
-    // force update
-    TIMx->EGR |= (1 << 0);
-
-    // Enable counter
-    TIMx->CR1 = (1 << 0);
-}
-
-// PWM signal generation with input frequency and duty cycle
-void PWM_setDutyCycle(TIMx_TypeDef *TIMx, uint32_t note_freq, uint32_t duty_cycle, uint32_t psc_val) {
-    // Set auto-reload register
-    // freq_TIM = 80 MHz / (psc_val + 1)
-    // arr_val = freq_TIM / note_freq - 1
-    int arr_val = (note_freq > 0) ? ((80000000 / (psc_val+1)) / note_freq - 1) : 0;
-    TIMx->ARR = arr_val;
-
-    // Set capture/compare register 1
-    TIMx->CCR1 = arr_val * duty_cycle / 100;
-
-    // Force update
-    TIMx->EGR |= (1 << 0);
-
-    // Reset count
-    TIMx->CNT = 0;
+  while(!(TIMx->SR & 1)); // Wait for UIF to go high
 }
--- mcu/lib/STM32L432KC_TIM.h
+++ mcu/lib/STM32L432KC_TIM.h
@@ -1,70 +1,19 @@
-/////////////////////////
-// STM32L432KC_TIM.h
-// Author: Georgia Tai, ytai@g.hmc.edu
-// Date: Sep 27, 2025
-// 
-// Header file for STM32L432KC_TIM.c, including TIM struct.
-/////////////////////////
+// STM32F401RE_TIM.h
+// Header for TIM functions
 
 #ifndef STM32L4_TIM_H
 #define STM32L4_TIM_H
 
-#include <stdint.h>
-
-///////////////////////////////////////////////////////////////////////////////
-// Definitions
-///////////////////////////////////////////////////////////////////////////////
-
-#define __IO volatile
-
-// Base addresses
-#define TIM15_BASE (0x40014000UL) // base address of TIM15
-#define TIM16_BASE (0x40014400UL) // base address of TIM16
-
-/**
-  * @brief Reset and Clock Control
-  */
-
-typedef struct
-{
-  __IO uint32_t CR1;         /*!< TIMx control register 1,                             Address offset: 0x00 */
-  __IO uint32_t CR2;         /*!< TIMx control register 2,                             Address offset: 0x04 */
-  __IO uint32_t SMCR;        /*!< TIMx slave mode control register,                    Address offset: 0x08 */
-  __IO uint32_t DIER;        /*!< TIMx DMA/interrupt enable register,                  Address offset: 0x0C */
-  __IO uint32_t SR;          /*!< TIMx status register,                                Address offset: 0x10 */
-  __IO uint32_t EGR;         /*!< TIMx event generation register,                      Address offset: 0x14 */
-  __IO uint32_t CCMR1;       /*!< TIMx capture/compare mode register 1,                Address offset: 0x18 */
-  __IO uint32_t CCMR2;       /*!< TIMx capture/compare mode register 2,                Address offset: 0x1C */
-  __IO uint32_t CCER;        /*!< TIMx capture/compare enable register,                Address offset: 0x20 */
-  __IO uint32_t CNT;         /*!< TIMx counter,                                        Address offset: 0x24 */
-  __IO uint32_t PSC;         /*!< TIMx prescalar,                                      Address offset: 0x28 */
-  __IO uint32_t ARR;         /*!< TIMx auto-related register,                          Address offset: 0x2C */
-  __IO uint32_t RCR;         /*!< TIMx repetition counter register,                    Address offset: 0x30 */
-  __IO uint32_t CCR1;        /*!< TIMx capture/compare register 1,                     Address offset: 0x34 */
-  __IO uint32_t CCR2;        /*!< TIMx capture/compare register 2,                     Address offset: 0x38 */
-  uint32_t      RESERVED1;   /*!< Reserved,                                            Address offset: 0x3C */
-  uint32_t      RESERVED2;   /*!< Reserved,                                            Address offset: 0x40 */
-  __IO uint32_t BDTR;        /*!< TIMx break and dead-time register,                   Address offset: 0x44 */
-  __IO uint32_t DCR;         /*!< TIMx DMA control register,                           Address offset: 0x48 */
-  __IO uint32_t DMAR;        /*!< TIMx DMA address for full transfer,                  Address offset: 0x4C */
-  __IO uint32_t OR1;         /*!< TIMx option register 1,                              Address offset: 0x50 */
-  uint32_t      RESERVED3;   /*!< Reserved,                                            Address offset: 0x54 */
-  uint32_t      RESERVED4;   /*!< Reserved,                                            Address offset: 0x58 */
-  uint32_t      RESERVED5;   /*!< Reserved,                                            Address offset: 0x5C */
-  __IO uint32_t OR2;         /*!< TIMx option register 2,                              Address offset: 0x60 */
-
-} TIMx_TypeDef; // TIMx struct for TIM15 and TIM16
-
-#define TIM15 ((TIMx_TypeDef *) TIM15_BASE)
-#define TIM16 ((TIMx_TypeDef *) TIM16_BASE)
+#include <stdint.h> // Include stdint header
+#include <stm32l432xx.h>
+#include "STM32L432KC_GPIO.h"
 
 ///////////////////////////////////////////////////////////////////////////////
 // Function prototypes
 ///////////////////////////////////////////////////////////////////////////////
 
-void initTIM(TIMx_TypeDef *TIMx, uint32_t psc_val);
-void delay_millis(TIMx_TypeDef * TIMx, uint32_t ms, uint32_t psc_val);
-void initPWM(TIMx_TypeDef *TIMx, uint32_t psc_val);
-void PWM_setDutyCycle(TIMx_TypeDef *TIMx, uint32_t note_freq, uint32_t duty_cycle, uint32_t psc_val);
+void initTIM(TIM_TypeDef * TIMx);
+void delay_millis(TIM_TypeDef * TIMx, uint32_t ms);
+void delay_micros(TIM_TypeDef * TIMx, uint32_t us);
 
 #endif
